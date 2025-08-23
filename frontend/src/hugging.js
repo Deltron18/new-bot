3f7c0d1c1af3ef8bace7116361316efded2dbb75
Deltron18 <karans0518@gmail.com>
2025-08-24 00:22:38 +0530

    all push

--- frontend/src/hugging.js
+++ frontend/src/hugging.js
@@ -1,21 +1,4 @@
-// import { prevUser } from "./context/Usercontext";
 
-//  export async function query() {
-// 	const response = await fetch(
-// 		  "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
-// 		{
-// 			headers: {
-// 				        Authorization: `Bearer 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`,
-
-// 				"Content-Type": "application/json",
-// 			},
-// 			method: "POST",
-// 			body: JSON.stringify({ inputs: prevUser.prompt }),
-// 		}
-// 	);
-// 	const result = await response.blob();
-// 	return result;
-// }
 
 
 // hugging.js
